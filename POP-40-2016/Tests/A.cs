929f62029320e8eea2a00542f9d06c9180c09d80
student <student@ftn.ns>
2017-10-16 12:31:43 +0200

    Initial commit

---
+++ POP-40-2016/Tests/A.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POP_40_2016
+{
+    public class A
+    {
+        private string naziv;
+
+        public string GetNaziv();
+        {
+            return this.internal void SetNAziv(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        naziv;
+        }
+
+    public void SetNaziv(string naziv);
+    {
+    this.naziv = naziv;
+    }
+
+    public string Ime { get; set; }
+
+    }
+}
