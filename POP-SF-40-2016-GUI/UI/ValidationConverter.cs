7d7d7c2438964f7b06e7361426b311b94b416af3
Nemanja <ngacic30@gmail.com>
2018-01-03 18:33:59 +0100

    Validacija za popust yes

---
+++ POP-SF-40-2016-GUI/UI/ValidationConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace POP_SF_40_2016_GUI.UI
+{
+    class ValidationConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,
+        System.Globalization.CultureInfo culture)
+        {
+            var sb = new StringBuilder();
+            var errors = value as ReadOnlyCollection<ValidationError>;
+            if (errors != null)
+            {
+                foreach (var e in errors.Where(e => e.ErrorContent != null))
+                { sb.AppendLine(e.ErrorContent.ToString()); }
+            }
+
+            return sb.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+        System.Globalization.CultureInfo culture)
+        { throw new NotImplementedException(); }
+    }
+}
---
+++ POP-SF-40-2016-GUI/UI/ValidationPopust.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace POP_SF_40_2016_GUI.UI
+{
+    class ValidationPopust:ValidationRule
+    {
+
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            try
+            {
+                string j = value as string;
+                int v = int.Parse(j);
+                if (v > 0 && v < 99)
+                    return new ValidationResult(true, null);
+                else
+                    return new ValidationResult(false, null);
+            }
+            catch (Exception)
+            {
+                return new ValidationResult(false, null);
+
+            }
+        }
+    }
+}
